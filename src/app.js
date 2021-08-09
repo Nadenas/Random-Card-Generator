f2c564d9a1ff5e4e0055ab21342343ed6d0757fb
Almudena De La Rosa <almudenaxico@gmail.com>
2021-08-09 16:06:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randonNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randonNumber];
+}
+
+function generateRandomSuit() {
+  let suit = ["spade", "club", "heart", "diamond"];
+  let randonSuit = Math.floor(Math.random() * suit.length);
+  return suit[randonSuit];
+}
